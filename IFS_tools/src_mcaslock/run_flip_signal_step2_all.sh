0948c48f2d50a2dba6ab1d9b34c44b9cdb6d05b3
Nimisha Limaye <nsl278@nyu.edu>
2021-04-29 12:18:53 +0400

    Add files via upload

---
+++ IFS_tools/src_mcaslock/run_flip_signal_step2_all.sh
@@ -0,0 +1,202 @@
+setenv TOOL dc
+setenv LIB 2ip
+
+setenv DESIGN c432
+setenv NET n325
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c499
+setenv NET n261
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c1355
+setenv NET n157 
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c1355
+setenv NET n259 
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN c1908
+setenv NET n353
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c2670
+setenv NET n472
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c3540
+setenv NET n1484
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c5315
+setenv NET n948
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c7552
+setenv NET n753
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b14_C
+setenv NET n2148
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b15_C
+setenv NET n2701
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b17_C
+setenv NET n3519
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b18_C
+setenv NET n10270
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b20_C
+setenv NET n2709
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b21_C
+setenv NET n2607
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b22_C
+setenv NET n2698
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv TOOL rc
+setenv LIB 2ip
+
+setenv DESIGN c432
+setenv NET n_229
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c499
+setenv NET n_141
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN c499
+setenv NET n_144
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN c499
+setenv NET n_145
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_3
+
+setenv DESIGN c499
+setenv NET n_150
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_4
+
+setenv DESIGN c499
+setenv NET n_181
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_5
+
+setenv DESIGN c499
+setenv NET n_182
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_6
+
+setenv DESIGN c1355
+setenv NET n_137
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN c1355
+setenv NET n_138
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN c1355
+setenv NET n_177
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_3
+
+setenv DESIGN c1908
+setenv NET n_213
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c2670
+setenv NET n_280
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN c3540
+setenv NET n_40
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN c3540
+setenv NET n_41
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN c5315
+setenv NET n_505
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN c5315
+setenv NET n_509
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN c7552
+setenv NET n_503
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b14_C
+setenv NET n_1114
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN b14_C
+setenv NET n_1116
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN b14_C
+setenv NET n_1117
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_3
+
+setenv DESIGN b14_C
+setenv NET n_1119
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_4
+
+setenv DESIGN b15_C
+setenv NET n_2393
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b17_C
+setenv NET n_2386
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b18_C
+setenv NET n_5461
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_$DESIGN
+
+setenv DESIGN b20_C
+setenv NET n_1322
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN b20_C
+setenv NET n_1334
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN b21_C
+setenv NET n_n900
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN b21_C
+setenv NET n_921
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
+setenv DESIGN b21_C
+setenv NET n_933
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_3
+
+setenv DESIGN b21_C
+setenv NET n_939
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_4
+
+setenv DESIGN b21_C
+setenv NET n_1369
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_5
+
+setenv DESIGN b22_C
+setenv NET n_1389
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_1
+
+setenv DESIGN b22_C
+setenv NET n_1400
+./run_flip_signal_step2.sh |tee log_${TOOL}_ifs_${DESIGN}_2
+
